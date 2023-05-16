a7f26e4e5a7ddb46920a7c42163c4615096a8897
AustinoBlack <baustinblack98@gmail.com>
2023-05-15 23:33:57 -0400

    added gather_counties(), which returns a list of all county names in the database

--- Cartography.py
+++ Cartography.py
@@ -1,5 +1,7 @@
 # A Python program to produce a map of VA and its counties
 
+import psycopg2
+
 class Color:
     def __init__(self, red, green, blue):
         self.red = red
@@ -8,6 +10,7 @@ class Color:
 
     def __str__(self):
         return "<path fill=\"rgb(" + str((self.red/100)*255) + "," + str((self.green/100)*255) + "," + str((self.blue/100)*255) + ")\""
+
 
 class County:
     def __init__(self, name, pop, zero, fifteen, thirty, fourtyfive, sixty):
@@ -26,11 +29,20 @@ class County:
     def __str__(self):
         return "Name: " + self.name + "\nPop: " + self.pop + "\nAge 0-14: " + self.zero + "\nAge 15-29: " + self.fifteen + "\nAge 30-44: " + self.thirty + "\nAge 45-60: " + self.fourtyfive + "\nAge 60+: " + self.sixty
 
-def Main():
-    county = County( "Richmond.City", "213735", "15.9", "28.8", "20", "18.3", "17.2" )
-    print( county )
 
-    y = Color( county.young, county.middle, county.old )
-    print( y )
+def gather_counties():
+    '''opens the vacensus table and returns a list of all county names from the county column'''
+    with psycopg2.connect( database="vacensus", user="austinoblack", password="(AUS.Data.1998)", host="localhost" ) as conn:
+        with conn.cursor() as cur:
+            cur.execute( 'SELECT county FROM vacensus' )
+            rv = [r[0] for r in cur.fetchall()]
+    conn.close()
+    return rv
+
+
+def Main():
+    names = gather_counties()
+    print( names )
+
 
 Main()
