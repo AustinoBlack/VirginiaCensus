c4023b50e945867fc8c1cf69b2b947df802e4c4d
AustinoBlack <baustinblack98@gmail.com>
2023-06-27 09:30:24 -0400

    altered the color class constructor to clarify the input data

--- version_C#/Cartography.cs
+++ version_C#/Cartography.cs
@@ -7,11 +7,15 @@ class Color
     public double green;
     public double blue;
 
-    public Color( double r, double g, double b )
+    // young - ages 0-30
+    // adult - ages 31-60
+    // senior - ages 60+
+    // Color creates a rgb color values from the population values of a County
+    public Color( double young, double adult, double senior )
     {
-        red = ((r/100)*255);
-        green = ((g/100)*255);
-        blue = ((b/100)*255);
+        red = ((young/100)*255);
+        green = ((adult/100)*255);
+        blue = ((senior/100)*255);
     }
 
     public override string ToString()
@@ -24,4 +28,15 @@ class Color
         Color Accomack = new Color( 33, 37, 28);
         Console.WriteLine( Accomack.ToString() );
     }
+}
+
+class County
+{
+    public string name;
+    public int population;
+    public int zero;
+    public int fifteen;
+    public int thirty;
+    public int fourtyfive;
+    public int sixty;
 }
