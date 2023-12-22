94af4dcc9a49db24a1823553f3ffae5087689107
austinoblack <baustinblack98@gmail.com>
2023-12-21 21:00:41 -0500

    quick hand test to ensure class declarations are correct. make configurations and test cases will come next. Will explore Makefiles or CMake.

--- version_C++/main.cpp
+++ version_C++/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "Color.h"
+#include "County.h"
 
 int main()
 {
     std::cout << "{Hello, World!}" << std::endl;
+    Color testcolor = Color(1, 2, 3);
+    County testcounty = County("test", 1, 2, 3, 4, 5, 6);
+
+    std::cout << testcolor.asString() << std::endl;
+    std::cout << testcounty.asString() << std::endl;
 }
