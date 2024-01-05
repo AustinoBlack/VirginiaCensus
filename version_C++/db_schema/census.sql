737494fac4e54ca774c22ec4e182073fce92f3eb
austinoblack <baustinblack98@gmail.com>
2024-01-05 17:07:59 -0500

    The majority of this work has been done on a raspberry pi that hosts a PostgresSQL database for my census data, this is the schema for the virgina 2010 census table.

---
+++ version_C++/db_schema/census.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS vacensus;
+
+CREATE TABLE vacensus(
+    county TEXT,
+    population TEXT,
+    group1 FLOAT(1), -- 0-15
+    group2 FLOAT(1), -- 16-30
+    group3 FLOAT(1), -- 31-45
+    group4 FLOAT(1), -- 46-60
+    group5 FLOAT(1)  -- 60+
+);
