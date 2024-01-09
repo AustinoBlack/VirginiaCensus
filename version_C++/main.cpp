2ad5dd0f487092dc045ed172374439145dbf1b42
austinoblack <baustinblack98@gmail.com>
2024-01-08 23:38:24 -0500

    added a few objects from the libpq library just to make sure everything compiles correctly.

--- version_C++/main.cpp
+++ version_C++/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include "Color.h"
 #include "County.h"
+#include "libpq-fe.h"
 
 int main()
 {
+    //stubs to test compilation
+    const char* conninfo;
+    PGconn* conn;
+
     std::cout << "{Hello, World!}" << std::endl;
     Color testcolor = Color(1, 2, 3);
     County testcounty = County("test", 1, 2, 3, 4, 5, 6);
