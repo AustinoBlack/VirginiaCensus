8fc6ebe007b198ee3854955677e1e681aeafe550
austinoblack <baustinblack98@gmail.com>
2024-01-09 15:22:33 -0500

    hand test for initial database connection and query execution.

--- version_C++/main.cpp
+++ version_C++/main.cpp
@@ -6,8 +6,37 @@
 int main()
 {
     //stubs to test compilation
-    const char* conninfo;
-    PGconn* conn;
+    const char* conninfo = "dbname=censusdata user=austinoblack password=(AUS.Census.1998) host=CensusData.local port=5432";
+    PGconn* conn = PQconnectdb(conninfo);
+
+    // Check if the connection is successful
+    if (PQstatus(conn) != CONNECTION_OK) {
+        // If not successful, print the error message and finish the connection
+        printf("Error while connecting to the database server: %s\n", PQerrorMessage(conn));
+
+        // Finish the connection
+        PQfinish(conn);
+
+        // Exit the program
+        exit(1);
+    }
+
+    // We have successfully established a connection to the database server
+    printf("Connection Established\n");
+    printf("Port: %s\n", PQport(conn));
+    printf("Host: %s\n", PQhost(conn));
+    printf("DBName: %s\n", PQdb(conn));
+
+    char* query = "SELECT county FROM vacensus WHERE population = 23022"; // should return Prince_Edward
+
+    //executes the query
+    PGresult *res = PQexec(conn, query);
+
+    //prints out the value returned
+    printf("%s\n", PQgetvalue(res, 0, 0));
+
+    // Close the connection and free the memory
+    PQfinish(conn);
 
     std::cout << "{Hello, World!}" << std::endl;
     Color testcolor = Color(1, 2, 3);
