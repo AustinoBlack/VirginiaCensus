e31d26faabbe7b7a960e7a085c346b5098a12f7f
austinoblack <baustinblack98@gmail.com>
2024-01-12 17:28:10 -0500

    moved DB connection logic to its own function

--- version_C++/main.cpp
+++ version_C++/main.cpp
@@ -3,10 +3,13 @@
 #include "County.h"
 #include "libpq-fe.h"
 
-int main()
+/* connect DB attempts to connect to a Postgres database provided by a given conninfo.
+ * - A successful connection will return the connection.
+ * - An unsuccessful connection will display an error and gracefully exit the program.
+ */
+PGconn* connectDB( const char* conninfo )
 {
-    //stubs to test compilation
-    const char* conninfo = "dbname=censusdata user=austinoblack password=(AUS.Census.1998) host=CensusData.local port=5432";
+    // Attempt Database Connection
     PGconn* conn = PQconnectdb(conninfo);
 
     // Check if the connection is successful
@@ -21,11 +24,20 @@ int main()
         exit(1);
     }
 
-    // We have successfully established a connection to the database server
+    // Print connection successful message and stats about what we connected to
     printf("Connection Established\n");
     printf("Port: %s\n", PQport(conn));
     printf("Host: %s\n", PQhost(conn));
     printf("DBName: %s\n", PQdb(conn));
+
+    // Return the connection
+    return conn;
+}
+
+int main()
+{
+    const char* conninfo = "dbname=censusdata user=austinoblack password=(AUS.Census.1998) host=CensusData.local port=5432";
+    PGconn* conn = connectDB( conninfo );
 
     char* query = "SELECT county FROM vacensus WHERE population = 23022"; // should return Prince_Edward
 
