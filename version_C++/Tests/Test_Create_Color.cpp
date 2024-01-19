a469119138662af24eceb2dfe92cf754de4be449
austinoblack <baustinblack98@gmail.com>
2024-01-19 01:51:49 -0500

    added a test case the tests the fuctionality of the color class

--- version_C++/Tests/Test_Create_Color.cpp
+++ version_C++/Tests/Test_Create_Color.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "../Color.h"
 
 // a sample program that creates a file with a color for testing file comparison logic and working directory structure
 // this will act as a template for future testing
 int main()
 {
-    std::string str = "Red";
     std::ofstream f( "Color_Output.txt", std::ifstream::trunc);
 
-    f << str;
+    Color c = Color( 100, 0, 0 );
+
+    f << c.asString();
 }
